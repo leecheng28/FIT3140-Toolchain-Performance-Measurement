b32b2dba1858f7cffc6b4958ca4458237101306a
Matthew Ready <matt-r@hotmail.com>
2017-04-08 23:36:12 +1000

    Integrated basic Firebase and dealt with messages

--- Spike1_FirebaseSerialport/server.js
+++ Spike1_FirebaseSerialport/server.js
@@ -1,4 +1,18 @@
+var admin = require("firebase-admin");
 var SerialPort = require('serialport');
+
+// Connect to firebase.
+admin.initializeApp({
+    credential: admin.credential.cert(require("./serviceAccountKey.json")),
+    databaseURL: "https://fit3140-team29-a2-c8d2a.firebaseio.com"
+});
+
+// Access the firebase database.
+var db = admin.database();
+
+// Get references to the areas we care about.
+var spikeRef = db.ref("spike1");
+
 // Pick the right arduino to connect to.
 console.log("Finding arduinos...");
 SerialPort.list(function (err, ports) {
@@ -39,11 +53,11 @@ function connectToArduino(arduinoCom) {
 
     var commandDesc = {
         /*StartByte: [Name, IsTerminatedBySpecificByte, MessageLengthOrTerminatorByte, Callback]*/
-        0xF9: ["ProtocolVersion", false, 2, function(){}],
-        0xF0: ["Sysex", true, 0xF7, function(){}],
-        0x90: ["DigitalIOMessage", false, 2, function(){}],
+        0xF9: ["ProtocolVersion", false, 2, onProtocolVersion],
+        0xF0: ["Sysex", true, 0xF7, onSysex],
+        0x90: ["DigitalIOMessage", false, 2, onDigitalPortData],
     }
-
+    
     var dataSoFar = new Buffer(0);
     port.on('data', function(data) {
     try {
@@ -70,4 +84,50 @@ function connectToArduino(arduinoCom) {
         console.log(e);
         }
     });
+}
+
+function onMotionEvent(isMotion) {
+    var time = Date.now();
+    console.log("Motion is now " + (isMotion ? "ON" : "OFF") + " at time " + time + ". Pushing to Firebase.");
+    spikeRef.push({
+       time: time,
+       isMotion: isMotion, 
+    });
+}
+
+function onProtocolVersion(data) {
+    console.log(" - ProtocolVersion received: major=" + data[1] + " minor=" + data[2]);
+}
+
+function onSysex(data, port) {
+    // Make sure this is the right type of sysex (0x79: query firmware).
+    if (data[1] != 0x79) {
+        console.log(" - Expecting query firmware sysex. Exiting.");
+        process.exit(1);
+    }
+    
+    // What is the name of the firmware reported? Should be something like 
+    // StandardFirmata.ino
+    var fwName = "";
+    for (var i = 4; i < data.length-1; i+=2) {
+        fwName += String.fromCharCode(data[i]);
+    }
+    
+    console.log(" - QueryFirmware Sysex received: major=" + data[2] + ", minor=" + data[3] + ", fwName=" + fwName);
+    console.log(" - Turning on pin 2 for reads");
+    
+    // Send a "set mode to input on pin 2" command (0xF4, 2, 0), then send a "enable data reporting" command (0xD0, 1)
+    port.write(new Buffer([0xF4, 2, 0, 0xD0, 1]), function (err) {
+        if (err !== null) {
+            console.log("Error turning on pin 2: " + err.message);
+            process.exit(1);
+        } else {
+            console.log(" - Sent turn on for pin 2. Setup complete. Please generate motion.\n");
+        }
+    });
+}
+
+// Called when a pin changes value.
+function onDigitalPortData(data) {
+    onMotionEvent((data[1] & 4) != 0);
 }
