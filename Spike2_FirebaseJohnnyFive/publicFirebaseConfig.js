a5ede55055a7c59171f04eb26b1e3bdaf7a11979
Li Cheng <lcheng3@student.unimelb.edu.au>
2017-04-22 20:24:05 +1000

    update folder name

---
+++ Spike2_FirebaseJohnnyFive/publicFirebaseConfig.js
@@ -0,0 +1,9 @@
+// Go to firebase console, click add to Web app on main page. Update object that looks like what you see below:
+module.exports = {
+	"apiKey": "AIzaSyBO22mG2_UyIgCHQT4AwUGv-ea5sO3olak",
+    "authDomain": "fit3140-team29-a2-c8d2a.firebaseapp.com",
+    "databaseURL": "https://fit3140-team29-a2-c8d2a.firebaseio.com",
+    "projectId": "fit3140-team29-a2-c8d2a",
+    "storageBucket": "fit3140-team29-a2-c8d2a.appspot.com",
+    "messagingSenderId": "994982227467"
+}
